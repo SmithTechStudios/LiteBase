feb7f439af8f12bfe1e20931cb41611cb6da1413
Tom Smith <tsmith@knowbe4.com>
2024-10-25 22:19:04 +0100

    Remove weather stuff

--- DemoApp/Program.cs
+++ DemoApp/Program.cs
@@ -1,5 +1,3 @@
-using Microsoft.Data.Sqlite;
-using Microsoft.EntityFrameworkCore;
 using LiteBase;
 using SqlLiteUI;
 
@@ -14,8 +12,8 @@ builder.Services.AddCors(options =>
     options.AddDefaultPolicy(policy =>
     {
         policy.AllowAnyOrigin()
-              .AllowAnyHeader()
-              .AllowAnyMethod();
+            .AllowAnyHeader()
+            .AllowAnyMethod();
     });
 });
 
@@ -30,11 +28,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
 
 app.MapGet("/weatherforecast", () =>
     {
@@ -54,63 +47,4 @@ app.UseStaticFiles();
 app.UseLiteBaseUi();
 app.UseLiteBaseEndpoint("db/blogging.db");
 
-app.MapGet("/databaseinfo", () =>
-{
-    using var db = new BloggingContext();
-
-    var x = db.Database
-        .SqlQuery<Row>($"""
-                        SELECT
-                            m.name as TableName,
-                            p.name as ColumnName,
-                            p.type as ColumnType
-                        FROM
-                            sqlite_master AS m
-                                JOIN
-                            pragma_table_info(m.name) AS p
-                        WHERE
-                            m.type = 'table'
-                        ORDER BY
-                            m.name,
-                            p.cid
-                        """).ToList();
-
-    return x;
-});
-
-app.MapGet("/tableinfo/{tableName}", (string tableName) =>
-{
-
-    using var db = new SqliteConnection("Data Source=db/blogging.db");
-    db.Open(); ;
-
-    var cmd = db.CreateCommand();
-    cmd.CommandText = $"SELECT * FROM {tableName.ToLower()}";
-
-    List<List<object>> items = new List<List<object>>();
-    var reader = cmd.ExecuteReader();
-    while (reader.Read())
-    {
-        var item = new List<Object>();
-        items.Add(item);
-
-        for (int i = 0; i < reader.FieldCount; i++)
-            item.Add(reader[i]);
-    }
-
-    return items;
-    // return Request.CreateResponse<List<object>>(HttpStatusCode.OK, items);
-
-    /*var x = db.Database.SqlQueryRaw<dynamic>($"SELECT * FROM {tableName.ToLower()}").ToList();
-    return x;*/
-});
-
-
 app.Run();
-
-record Row(string TableName, string ColumnName, string? ColumnType);
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
