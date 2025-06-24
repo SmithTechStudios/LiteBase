ee232b3c5934b907e20158a773b4180d15d91d5b
Tom Smith <tom.smith2711@gmail.com>
2025-06-24 14:43:13 +0100

    update!

--- LiteBase/ui/src/main.ts
+++ LiteBase/ui/src/main.ts
@@ -5,12 +5,8 @@ import App from './App.vue'
 
 
 import PrimeVue from 'primevue/config';
-import Aura from '@primevue/themes/aura';
-import Button from 'primevue/button'
-import ProgressSpinner from 'primevue/progressspinner'
-import InputText from 'primevue/inputtext'
+import Aura from '@primeuix/themes/aura';
 
-import 'primeicons/primeicons.css'
 
 const app = createApp(App);
 app.use(PrimeVue, {
@@ -18,10 +14,6 @@ app.use(PrimeVue, {
         preset: Aura
     }
 });
-
-app.component('Button', Button);
-app.component('ProgressSpinner', ProgressSpinner);
-app.component('InputText', InputText);
 
 // Add VueQueryPlugin to your app
 app.use(VueQueryPlugin);
